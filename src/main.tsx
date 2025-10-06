7dcf69c9ece0c33f25b09f3158f609019ae938b7
Sammy <sammyanagolum@gmail.com>
2025-10-06 19:37:38 +0530

    Import basename from env

--- src/main.tsx
+++ src/main.tsx
@@ -6,10 +6,12 @@ import './index.css'
 import App from './App.tsx'
 import { ThemeProvider } from './providers/theme/theme-provider.tsx';
 
+const BASENAME = import.meta.env.VITE_APP_BASENAME ?? "/";
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider>
-      <BrowserRouter basename='/portal'>
+      <BrowserRouter basename={BASENAME}>
         <App />
         <Toaster position="top-right" richColors />
       </BrowserRouter>
