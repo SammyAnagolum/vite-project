817de4aaa1e91025edb63ad5663f0149acd008f2
Sammy <sammyanagolum@gmail.com>
2025-10-07 15:28:05 +0530

    Remove unused icons

--- src/lib/brand.ts
+++ src/lib/brand.ts
@@ -1,5 +1,5 @@
 // src/brand.ts
-export { default as LogoHoriz } from "@/assets/brand/sahamati-primary.svg";
-export { default as LogoHorizDark } from "@/assets/brand/sahamati-primary-white.png";
-export { default as LogoVert } from "@/assets/brand/sahamati-vertical.svg";
 export { default as LogoMark } from "@/assets/brand/sahamati-icon.svg";
+export { default as LogoHorizDark } from "@/assets/brand/sahamati-primary-white.png";
+export { default as LogoHoriz } from "@/assets/brand/sahamati-primary.svg";
+export { default as LogoVert } from "@/assets/brand/sahamati-vertical.svg";
