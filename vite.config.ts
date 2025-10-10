d41bab5841f48e7d07c99af710ed62fc96575c2d
Sammy <sammyanagolum@gmail.com>
2025-10-10 15:45:39 +0530

    Switch from 'portal' to 'new-portal'

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { fileURLToPath, URL } from "node:url";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/portal/",
+  base: "/new-portal/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
